0ebf94d97f5a4cc24a36f5ff53e754ba105b467d
QuocAn1301 <137316838+QuocAn1301@users.noreply.github.com>
2023-10-07 02:05:14 +0700

    Thêm tính năng mới

--- database/migrations/2023_10_06_174459_create_news.php
+++ database/migrations/2023_10_06_174459_create_news.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateNewsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('news', function (Blueprint $table) {
-            $table->id();
+            $table->id('NewsID');
+            $table->unsignedBigInteger('CategoryId');
+            $table->string('NewsName');
+            $table->text('Content');
             $table->timestamps();
+
+            // Khóa ngoại liên kết với bảng categories
+            $table->foreign('CategoryId')->references('CategoryId')->on('categories');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('news');
     }
-};
+}
+
