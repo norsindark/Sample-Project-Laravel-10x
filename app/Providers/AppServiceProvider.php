db0ec9325f2bec47511638ed744bd5099f089ab6
Nguyen Tuan <norsinlight@gmail.com>
2023-10-12 17:42:56 +0700

    import data of categories and products to client

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Products;
+use App\Models\Categories;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
         Validator::extend('checkUserName', function ($attribute, $value, $parameters, $validator) {
             // Sử dụng biểu thức chính quy để kiểm tra chuỗi
             return preg_match('/^[a-zA-Z0-9_ -]+$/', $value);
+        });
+
+        // Share all data
+        $products = Products::all();
+        $categories = Categories::all();
+        View::composer('*', function ($view) use ($products, $categories) {
+            $view->with(['products' => $products, 'categories' => $categories]);
         });
     }
 }
