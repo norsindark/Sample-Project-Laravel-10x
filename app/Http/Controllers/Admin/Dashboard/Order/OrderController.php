54ed94ee1b207dbb64ea03e9a567823facff600f
Nguyen Tuan <norsinlight@gmail.com>
2023-10-13 16:36:47 +0700

    update delete cart_items when create order success

--- app/Http/Controllers/Admin/Dashboard/Order/OrderController.php
+++ app/Http/Controllers/Admin/Dashboard/Order/OrderController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Admin\Dashboard\Order;
 use App\Http\Controllers\Controller;
 use App\Models\orders;
 use App\Models\order_details;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('dashboard.order.order');
     }
 
@@ -17,7 +19,7 @@ class OrderController extends Controller
     {
 
         $cartItems = json_decode($request->input('cartItems'), true);
-        $totalPrice = $request->input('totalPrice'); 
+        $totalPrice = $request->input('totalPrice');
         // dd($totalPrice);
 
         // if (auth()->user()->name === null || auth()->user()->address === null || auth()->user()->phone === null) {
@@ -29,18 +31,21 @@ class OrderController extends Controller
         // $order->recipient = auth()->user()->name;
         // $order->address = auth()->user()->address;
         // $order->phone = auth()->user()->phone;
-        $order->user_id = auth()->user()->id; 
+        $order->user_id = auth()->user()->id;
         $order->save();
 
         foreach ($cartItems as $cartItem) {
             // dd($cartItem);
             $orderDetail = new order_details();
             $orderDetail->order_id = $order->id;
-            $orderDetail->product_id = $cartItem['product_id']; 
+            $orderDetail->product_id = $cartItem['product_id'];
             $orderDetail->quantity = $cartItem['quantity'];
-            $orderDetail->price = $cartItem['price']; 
+            $orderDetail->price = $cartItem['price'];
             $orderDetail->save();
         }
+
+        $cartItemIds = array_column($cartItems, 'id');
+        DB::table('cart_items')->whereIn('id', $cartItemIds)->delete();
 
         return redirect()->route('thanh-toan')->with('success', 'Đã đặt hàng thành công!');
     }
